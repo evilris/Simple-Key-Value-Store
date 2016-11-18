8dc0780df9495e1695aad40ec79508fc17f515b1
evilris <tomasz@lukawski.me>
2016-11-18 22:47:42 +0100

    Added StoreHadnler and tests for it

---
+++ src/main/java/me/lukawski/skvs/store/StoreHandler.java
@@ -0,0 +1,17 @@
+package me.lukawski.skvs.store;
+
+import me.lukawski.skvs.operations.OperationResult;
+import me.lukawski.skvs.operations.results.InformationOperationResult;
+
+/**
+ * Created by EvilRIS on 18.11.2016.
+ */
+public class StoreHandler {
+    public static StoreHandler getLocalInstance() {
+        return new StoreHandler();
+    }
+
+    public OperationResult process(String add_message) {
+        return new InformationOperationResult();
+    }
+}
---
+++ src/test/java/OperationHandleTests.java
@@ -0,0 +1,19 @@
+import me.lukawski.skvs.operations.OperationResult;
+import me.lukawski.skvs.store.StoreHandler;
+import org.junit.Assert;
+import org.junit.Test;
+/**
+ * Created by EvilRIS on 18.11.2016.
+ */
+public class OperationHandleTests {
+
+    public static final String OK_MESSAGE = "OK";
+    public static String add_message = "ADD KEY VALUE";
+    public static String get_message = "GET KEY";
+    @Test
+    public void addToStoreAndRetriveSameItem(){
+        StoreHandler store = StoreHandler.getLocalInstance();
+        OperationResult result = store.process(add_message);
+        Assert.assertEquals(result.getOperationMesage(), OK_MESSAGE);
+    }
+}
